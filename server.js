025b59af75fa4e50000542ebdbffefa75cd82dce
Ian Goodwin <iangoodwin173@gmail.com>
2023-05-18 20:21:33 -0400

    created user and project models

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ const app = express();
 
 const port = 3001;
 
+const exphbs = require('express-handlebars');
+
 const routes = require('./controllers/api/index');
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 //middleware
 app.use(express.static('public'));
@@ -16,7 +21,7 @@ app.get('/hi', (req, res)=>{
 res.send('Task Manager')
 })
 
-app.use("./api/v1/index", tasks)
+// app.use("./api/v1/index", tasks)
 
 
 // app.get('/api/v1/tasks') - get all the tasks 
@@ -24,6 +29,8 @@ app.use("./api/v1/index", tasks)
 // app.get('/api/v1/tasks/: id') - get single task
 // app.patch('/api/v1/tasks/: id') - update task
 // app.delete('/api/v1/tasks/: id') - delete task
+
+
 
 
 
